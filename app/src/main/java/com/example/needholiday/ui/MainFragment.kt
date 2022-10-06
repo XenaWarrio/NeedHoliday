497b4a191cfa365081cb16d1724000597d6e62e8
Xena <queen.de.xena@gmail.com>
2022-10-06 15:08:34 +0200

    Add tests

--- app/src/main/java/com/example/needholiday/ui/MainFragment.kt
+++ app/src/main/java/com/example/needholiday/ui/MainFragment.kt
@@ -1,31 +1,23 @@
 package com.example.needholiday.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
+import com.example.needholiday.R
 import com.example.needholiday.constants.LANGUAGE_CODE
 import com.example.needholiday.databinding.FragmentMainBinding
 import com.example.needholiday.prefs
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.fragment_main) {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMainBinding.bind(view)
+
         setUpViews()
         setUpListeners()
     }
--- app/src/test/java/com/example/needholiday/ExampleUnitTest.kt
+++ app/src/test/java/com/example/needholiday/ExampleUnitTest.kt
@@ -1,17 +1,46 @@
 package com.example.needholiday
 
+import androidx.test.core.app.ApplicationProvider
+import com.example.needholiday.repository.SharedPref
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.robolectric.RobolectricTestRunner
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(
+    RobolectricTestRunner::class)
 class ExampleUnitTest {
+    @Mock
+    lateinit var sp: SharedPref
+
+     private var sp1: SharedPref? = null
+
+    @Before
+    fun initMock() {
+        MockitoAnnotations.openMocks(this)
+    }
+    @Before
+    fun init(){
+        sp1 = SharedPref(ApplicationProvider.getApplicationContext())
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testSharedPref() {
+        Mockito.`when`(sp.language).thenReturn("uk")
+        assertEquals(sp.language, "uk")
+    }
+
+    @Test
+    fun testDefaultSharedPrefValue() {
+        assertEquals(sp1?.language, "en")
     }
 }
