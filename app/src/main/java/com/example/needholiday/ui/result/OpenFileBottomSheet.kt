5e83182c8e085438f1bac15606b44103a6d713a8
vipulasri <vipulasri.2007@gmail.com>
2022-10-27 10:55:36 -0400

    fix: bottomsheet behavior and webview height to show the html content in bottomsheet

--- app/src/main/java/com/example/needholiday/ui/result/OpenFileBottomSheet.kt
+++ app/src/main/java/com/example/needholiday/ui/result/OpenFileBottomSheet.kt
@@ -1,11 +1,21 @@
 package com.example.needholiday.ui.result
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.FrameLayout
 import androidx.navigation.fragment.navArgs
 import com.example.needholiday.databinding.BottomSheetOpenFileBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
@@ -13,6 +23,18 @@ class OpenFileBottomSheet : BottomSheetDialogFragment() {
     private var _binding: BottomSheetOpenFileBinding? = null
     private val binding get() = _binding!!
     private val args : OpenFileBottomSheetArgs by navArgs()
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialog.setOnShowListener {
+            val bottomDialog = it as BottomSheetDialog
+            val bottomSheet = bottomDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout?
+            bottomSheet?.let {
+                BottomSheetBehavior.from(it).state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        }
+        return dialog
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +51,16 @@ class OpenFileBottomSheet : BottomSheetDialogFragment() {
 
         with(binding){
             webView.settings.allowFileAccess = true
+            webView.webViewClient = object: WebViewClient() {
+                override fun onReceivedError(
+                    view: WebView?,
+                    request: WebResourceRequest?,
+                    error: WebResourceError?
+                ) {
+                    super.onReceivedError(view, request, error)
+                    Log.e("OpenFileBottomSheet", "error: ${error?.description?: ""}")
+                }
+            }
             webView.loadUrl(args.fileUri)
         }
     }
